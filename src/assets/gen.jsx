16861b7bb4342299c6e8940685709d7efaa50b7e
MirgudeSaikrishna <146564634+MirgudeSaikrishna@users.noreply.github.com>
2025-04-17 01:48:29 +0530

    Update gen.jsx

--- src/assets/gen.jsx
+++ src/assets/gen.jsx
@@ -13,7 +13,7 @@ const Gen = () => {
         setLoading(true);
 
         try {
-            const eventSource = new EventSource('https://blogai-backend.onrender.com' + encodeURIComponent(topic));
+            const eventSource = new EventSource('https://blogai-backend.onrender.com/generate-content?topic=' + encodeURIComponent(topic));
             eventSource.onmessage = (event) => {
                 if (event.data === '[DONE]') {
                     eventSource.close();
